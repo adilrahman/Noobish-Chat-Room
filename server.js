09bc87c196d644ae9a64beb0ef66862ce298c900
adilrahman <adilrahman20008@gmail.com>
2020-08-19 20:48:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 
 const io = require('socket.io')(3000);
 
+var UserList = {};
+
 io.on('connection', socket => {
-	console.log("Connnected");
-	socket.emit('somthing',{data : "nothing"});
+	console.log(`Connnected  : ${socket.id} `);
+	socket.emit('somthing', "nothing");
+	UserList[socket.id] = "name";
+
+	socket.broadcast.emit('join',UserList[socket.id]);
+	
+	socket.on("send-message", data => {
+		let name = data['name'];
+		let msg = data['msg'];
+		console.log(`${name} : ${msg}`);
+		socket.broadcast.emit('new-message', data);
+	});
 
 	socket.on('disconnect',() => {
-		console.log("Disconnected");
+		delete UserList[socket.id];
+		console.log("Disconnected ${socket.id}");
+		socket.broadcast.emit('left',socket.id);
 	});
 });
 
