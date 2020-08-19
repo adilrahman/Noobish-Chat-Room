5a50a757a74021754ca8435f86eccaa7769d1149
adilrahman <adilrahman20008@gmail.com>
2020-08-20 01:24:02 +0530

    First phase completed

--- js/script.js
+++ js/script.js
@@ -1,39 +1,82 @@
 
 const form  = document.querySelector('form');
+const messageBlock = document.querySelector('.MessagesBlock');
 
 const name = prompt("Enter you name : ");
 
 if(name !== null && name !== "")
 {
-
 	const socket = io('http://localhost:3000');
 	socket.emit('new-User', name);
 
-form.addEventListener('submit', event => {
-	event.preventDefault();
-	let formData = new FormData(form);
-	let msg = formData.get('msg');
-	console.log(`You : ${msg}`);
-	socket.emit("send-message",{ name , msg});
-	form.reset();
-});
+	form.addEventListener('submit', event => {
+		event.preventDefault();
+		let formData = new FormData(form);
+		let msg = formData.get('msg');
+		console.log(`You : ${msg}`);
+		if(msg.trim() !== "" ){
+			socket.emit("send-message",{ name , msg});
+			
+			let divTag = document.createElement('div');
+			let nameTag = document.createElement('h6');
+			let messageTag = document.createElement('p');
+			
+			divTag.setAttribute('class',"Message bg-light");
 
-socket.on('join', name => {
-	console.log(`${name} joined`);
-});
+			nameTag.textContent = "You";
+			messageTag.textContent = msg;
+			divTag.appendChild(nameTag);
+			divTag.appendChild(messageTag);
+			messageBlock.appendChild(divTag);
+		
+			form.reset();
+		}
+	});
 
-socket.on('new-message', data => {
-	console.log(`${data.name} : ${data.msg}`);
-});
+	socket.on('join', name => {
+		console.log(`${name} joined`);
+		let divTag = document.createElement('div');
+		let smallTag = document.createElement('small');
+		smallTag.textContent = `${name} joined`;
+		divTag.setAttribute('class' , 'join  text-success');
 
-socket.on('left',who => {
-	console.log(`${who} lefted`);
-});
+		divTag.appendChild(smallTag);
+		messageBlock.appendChild(divTag);
 
-socket.on('somthing', data => {
+	});
 
-	console.log(data);
+	socket.on('new-message', data => {
+		console.log(`${data.name} : ${data.msg}`);
+		let divTag = document.createElement('div');
+		let nameTag = document.createElement('h6');
+		let messageTag = document.createElement('p');
 
-});
+		divTag.setAttribute('class',"Message bg-light");
+		
+		nameTag.textContent = data.name;
+		messageTag.textContent = data.msg;
+		divTag.appendChild(nameTag);
+		divTag.appendChild(messageTag);
+		messageBlock.appendChild(divTag);
+	});
+
+	socket.on('left',who => {
+		let divTag = document.createElement('div');
+		let smallTag = document.createElement('small');
+		smallTag.textContent = `${who} left`;
+		divTag.setAttribute('class' , 'disconnect text-danger');
+
+		divTag.appendChild(smallTag);
+		messageBlock.appendChild(divTag);
+
+
+		console.log(`${who} lefted`);
+	});
+
+	socket.on('somthing', data => {
+	
+		console.log(data);
+
+	});
 
 }
