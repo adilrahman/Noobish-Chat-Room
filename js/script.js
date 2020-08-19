581297af40bbff1c1b40bcd44b38df5419891032
adilrahman <adilrahman20008@gmail.com>
2020-08-19 20:10:04 +0530

    Update server.js script.js index.html

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 
 const socket = io('http://localhost:3000');
+const form  = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+	event.preventDefault();
+	let formData = new FormData(form);
+	let msg = formData.get('msg');
+	console.log(msg);
+	form.reset();
+});
+
+
+
 
 socket.on('somthing',data => {
 
 	console.log(data.data);
 
 });
+
 
--- server.js
+++ server.js
@@ -4,5 +4,9 @@ const io = require('socket.io')(3000);
 io.on('connection', socket => {
 	console.log("Connnected");
 	socket.emit('somthing',{data : "nothing"});
+
+	socket.on('disconnect',() => {
+		console.log("Disconnected");
+	});
 });
 
